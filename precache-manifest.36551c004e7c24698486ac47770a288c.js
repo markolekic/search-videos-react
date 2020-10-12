8d442c9fc0d81418db6dcdba2fec52387fd79305
Marko <markolekic985@gmail.com>
2020-10-12 16:30:27 +0200

    Updates

---
+++ precache-manifest.36551c004e7c24698486ac47770a288c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "645275eb6f9d8e66dec312669c1fc000",
+    "url": "/search-videos-react/index.html"
+  },
+  {
+    "revision": "a4fb93e557f746573745",
+    "url": "/search-videos-react/static/css/main.920abe7c.chunk.css"
+  },
+  {
+    "revision": "ff903ac343581c56f786",
+    "url": "/search-videos-react/static/js/2.de7f7896.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/search-videos-react/static/js/2.de7f7896.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4fb93e557f746573745",
+    "url": "/search-videos-react/static/js/main.02826773.chunk.js"
+  },
+  {
+    "revision": "111c8a60f89eb7c164d2",
+    "url": "/search-videos-react/static/js/runtime-main.e4913ddb.js"
+  }
+]);
